05e1f23435b0e792afeac7e675741dad41f428b3
kwojtas172 <k.wojtas172@gmail.com>
2021-10-11 14:20:13 +0200

    classic functions -> arrow functions

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 require('dotenv').config();
 
 const MongoClient = require('mongodb').MongoClient;
-const url = process.env.DB_URL; //url for sb from .env
+const url = process.env.DB_URL; //url for db from .env
 const apiKey = process.env.API_KEY; //for authorization from .env
 
 
@@ -21,15 +21,15 @@ app.get('/', (req, res) => {
 //getting all documents from db
 app.get('/locations', (req, res) => {
     if(req.headers.apikey === apiKey) { //authorization
-        MongoClient.connect(url, function(err, dbs) {
-            if (err) throw err;
-            const db = dbs.db("find-andrew");
-            db.collection("locations").find({}).toArray((err, result) => {
+        MongoClient.connect(url, (err, dbs) => {
+                if (err) throw err;
+                const db = dbs.db("find-andrew");
+                db.collection("locations").find({}).toArray((err, result) => {
                     if (err) throw err;
                     dbs.close();
                     res.json(result);
                 });
-          });
+            });
     } else res.json({res: 'access denied'}); //no authorization response
   });
 
@@ -39,18 +39,18 @@ app.get('/locations', (req, res) => {
   app.post('/locations', (req, res) => {
     const data = req.body; //new object
     if(req.headers.apikey === apiKey) { //authorization
-        MongoClient.connect(url, function(err, dbs) {
-            if (err) throw err;
-            const db = dbs.db("find-andrew");
-            db.collection("locations").insertOne(data, (err, result) => {
+        MongoClient.connect(url, (err, dbs) => {
+                if (err) throw err;
+                const db = dbs.db("find-andrew");
+                db.collection("locations").insertOne(data, (err, result) => {
                     if (err) throw err;
                     dbs.close();
                     res.json(result);
                 });
-          });
+            });
     } else res.json({res: 'acces denied'}); //no authorization response
   })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 })
